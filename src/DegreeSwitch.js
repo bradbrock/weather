e74f9f1439b2223abd7958ba5790432392a14b8a
Brad Brock <bbrock@fossil.com>
2020-09-01 20:33:33 -0500

    Correct temp unit labels

--- src/DegreeSwitch.js
+++ src/DegreeSwitch.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DegreeSwitch = ({degreeType, switchValue}) => {
     return (
         <div className="degree-switch d-flex pt-2">
-            <div className="pr-2">°C</div>
+            <div className="pr-2">°F</div>
             <div className="custom-control custom-switch">
                 <input
                     type="checkbox"
@@ -13,7 +13,7 @@ const DegreeSwitch = ({degreeType, switchValue}) => {
                 />
                 <label className="custom-control-label" htmlFor="customSwitch1"> </label>
             </div>
-            <div>°F</div>
+            <div>°C</div>
         </div>
     )
 }
